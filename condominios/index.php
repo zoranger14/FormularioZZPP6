96fa25e5d3f8c92fb3d2e2ef88e052f201a168bb
zoranger14 <62315256+zoranger14@users.noreply.github.com>
2020-07-29 16:43:16 -0400

    mi primer commit

--- condominios/index.php
+++ condominios/index.php
@@ -69,7 +69,102 @@ else
 				 		<td><?php echo $reg['cod_postal']; ?></td>
 			 		</tr>
 	 			<?php } ?>
-		</table>
+	 	</table>
+
+	 			</br><table width="70%" bgcolor="DEF8FF" align="center">
+					<tr align="center">
+						<th colspan="6" bgcolor="80D6ED"><h2>Inmuebles con estacionamiento y desocupados</h2></th>
+					</tr>
+						<tr align="center">
+							<td><b>ID</b></td>
+							<td><b>Identificacion</b></td>
+							<td><b>Estacionamiento</b></td>
+							<td><b>Estado</b></td>
+							<td><b>Tipo</b></td>
+							<td><b>Cod. Postal</b></td>
+						</tr>
+
+					<?php 
+		 			$sql="SELECT * FROM inmuebles WHERE estacionamiento='Si' AND status='Desocupado'";
+		 			$res=mysqli_query($conex,$sql);
+
+		 			while($reg=mysqli_fetch_array($res))
+		 			{
+		 		?>
+				 		<tr align="center">
+					 		<td bgcolor="5EE0C6"><?php echo $reg['id']; ?></td>
+					 		<td><?php echo $reg['idem']; ?></td>
+					 		<td><?php echo $reg['estacionamiento']; ?></td>
+					 		<td><?php echo $reg['status']; ?></td>
+					 		<td><?php echo $reg['tipo']; ?></td>
+					 		<td><?php echo $reg['cod_postal']; ?></td>
+				 		</tr>
+	 			<?php } ?>
+				</table>
+
+	 			</br><table width="70%" bgcolor="DEF8FF" align="center">
+					<tr align="center">
+						<th colspan="6" bgcolor="80D6ED"><h2>Inmuebles que estan desocupadas y no tienen estacionamiento</h2></th>
+					</tr>
+						<tr align="center">
+							<td><b>ID</b></td>
+							<td><b>Identificacion</b></td>
+							<td><b>Estacionamiento</b></td>
+							<td><b>Estado</b></td>
+							<td><b>Tipo</b></td>
+							<td><b>Cod. Postal</b></td>
+						</tr>
+
+					<?php 
+		 			$sql="SELECT * FROM inmuebles WHERE estacionamiento='No' AND status='Desocupado'";
+		 			$res=mysqli_query($conex,$sql);
+
+		 			while($reg=mysqli_fetch_array($res))
+		 			{
+		 		?>
+				 		<tr align="center">
+					 		<td bgcolor="5EE0C6"><?php echo $reg['id']; ?></td>
+					 		<td><?php echo $reg['idem']; ?></td>
+					 		<td><?php echo $reg['estacionamiento']; ?></td>
+					 		<td><?php echo $reg['status']; ?></td>
+					 		<td><?php echo $reg['tipo']; ?></td>
+					 		<td><?php echo $reg['cod_postal']; ?></td>
+				 		</tr>
+	 			<?php } ?>
+				</table>
+
+				</br><table  bgcolor="DEF8FF" align="center">
+					<tr align="center">
+						<th colspan="6" bgcolor="80D6ED"><h2>Contar inmuebles</h2></th>
+					</tr>
+						<tr align="center">
+							<td><b>Con estacionamiento</b></td>
+							<td><b>Sin estacionamiento</b></td>
+							<td><b>Ocupados</b></td>
+							<td><b>Desocupados</b></td>
+						</tr>
+
+					<?php 
+		 			$sql= "SELECT
+		 			count(case when estacionamiento='Si' then 1 end),
+		 			count(case when estacionamiento='no' then 1 end),
+		 			count(case when status='Ocupado' then 1 end),
+		 			count(case when status='Desocupado' then 1 end)
+		 			FROM inmuebles";
+		 			$res=$conex->query($sql);
+
+		 			$reg= $res->fetch_array();
+		 			{
+		 		?>
+				 		<tr align="center">
+					 		<td><?php echo $reg['0']; ?></td>
+					 		<td><?php echo $reg['1']; ?></td>
+					 		<td><?php echo $reg['2']; ?></td>
+					 		<td><?php echo $reg['3']; ?></td>
+					 		
+				 		</tr>
+	 			<?php } ?>
+				</table>
 
 </body>
 </html>
